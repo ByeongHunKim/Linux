205de1a16d23fb3c466b76ee155917895b42a646
ByeongHunKim <rkwhr912@naver.com>
2023-01-13 11:03:09 +0900

    1월 13일 금요일 star 별 찍기

--- Shell/star.sh
+++ Shell/star.sh
@@ -4,12 +4,16 @@
 # 기본적으로 echo명령이실행되고 나면 줄바꿈이 되어 진다.  줄바꿈을 하지않고 출력하려면 echo -n 변수 
 
 
-for((i=0;i<3;i++)) # https://jhnyang.tistory.com/191
+for((i=1; i<=3; i++)) 
 do
-for((j=0;j<i+1;j++))
-        do
-echo -n "*" # https://taesun1114.tistory.com/entry/Shell-script-%EA%B8%B0%EB%B3%B8-%EB%AC%B8%EB%B2%95-1echo%EC%99%80-%EB%B3%80%EC%88%98
-        done
-echo ""
+for((j=4-$i; j>0; j--))
+  do
+    echo -n " " # 찍히는 별 앞에 공백
+  done
+for((k=1; k<$i+1; k++)) 
+  do
+    echo -n "*" # 별 찍기
+  done
+  echo "" # 줄바꿈
 done
 
